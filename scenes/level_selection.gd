84ab3a09e6c820a7699c7cf37fc61d209f23013e
Nikel <494311870@qq.com>
2026-02-01 20:43:57 +0800

    feat: 关卡选择界面和主界面

---
+++ scenes/level_selection.gd
@@ -0,0 +1,34 @@
+extends CanvasLayer
+
+signal level_selected(level_index: int)
+signal back_pressed
+
+@onready var grid_container: GridContainer = %GridContainer
+@onready var btn_back: Button = %BtnBack
+
+func _ready() -> void:
+	btn_back.pressed.connect(func(): back_pressed.emit())
+
+func setup_levels(level_paths: Array[String]) -> void:
+	# 清理旧按钮
+	for child in grid_container.get_children():
+		child.queue_free()
+	
+	# 为每个关卡创建按钮
+	for i in range(level_paths.size()):
+		var btn = Button.new()
+		var path = level_paths[i]
+		var level_name = path.get_file().get_basename().replace("level_", "")
+		
+		btn.text = level_name
+		btn.custom_minimum_size = Vector2(80, 80)
+		btn.set("theme_override_font_sizes/font_size", 24)
+		
+		btn.pressed.connect(func(): level_selected.emit(i))
+		grid_container.add_child(btn)
+
+func show_selection() -> void:
+	show()
+
+func hide_selection() -> void:
+	hide()
